b1e05bb5e7979fd9fdd9a2965b73ff2f276f4e72
Ryan Barrett <ryan@seldonplan.com>
2021-02-22 17:39:43 +0100

    Added signer tool to publish with profiles for both OSes

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 dotnet publish src/ProofOfTestApi/ProofOfTestApi.csproj -o publish -p:PublishProfile=FolderProfileProofOfTestApi.csproj
 dotnet publish src/HolderAppApi/HolderAppApi.csproj -o publish/HolderAppApi -p:PublishProfile=FolderProfileHolderAppApi.csproj
 dotnet publish src/VerifierAppApi/VerifierAppApi.csproj -o publish/VerifierAppApi -p:PublishProfile=FolderProfileVerifierAppApi.csproj
+dotnet publish src/CmsSigner/CmsSigner.csproj -o publish/Tools/CmsSigner -p:PublishProfile=Publish-Linux-x64
