30672b051d9b4bb95204bbbabb869c3f8c623131
Ryan Barrett <ryan@seldonplan.com>
2021-02-25 14:46:34 +0100

    Fixes for the linux build.

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
 dotnet build src/ProofOfTestApi/ProofOfTestApi.csproj
-dotnet build src/HolderAppApi/HolderAppApi.csproj
-dotnet build src/VerifierAppApi/VerifierAppApi.csproj
+REM dotnet build src/HolderAppApi/HolderAppApi.csproj
+REM dotnet build src/VerifierAppApi/VerifierAppApi.csproj
+dotnet build src/CmsSigner/CmsSigner.csproj
--- publish.bat
+++ publish.bat
@@ -1,4 +1,4 @@
 dotnet publish src/ProofOfTestApi/ProofOfTestApi.csproj -o publish/ProofOfTestApi
-dotnet publish src/HolderAppApi/HolderAppApi.csproj -o publish/HolderAppApi
-dotnet publish src/VerifierAppApi/VerifierAppApi.csproj -o publish/VerifierAppApi
+REM dotnet publish src/HolderAppApi/HolderAppApi.csproj -o publish/HolderAppApi
+REM dotnet publish src/VerifierAppApi/VerifierAppApi.csproj -o publish/VerifierAppApi
 dotnet publish src/CmsSigner/CmsSigner.csproj -o publish/Tools/CmsSigner -p:PublishProfile=Publish-Win-x64
