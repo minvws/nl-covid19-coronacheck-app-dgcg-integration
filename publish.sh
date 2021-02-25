bde937793056600e767dfb5aca6615961fbcd88a
Ryan Barrett <ryan@seldonplan.com>
2021-02-25 09:37:04 +0100

    Fixed error in the build script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
-dotnet publish src/ProofOfTestApi/ProofOfTestApi.csproj -o publish
+dotnet publish src/ProofOfTestApi/ProofOfTestApi.csproj -o publish/ProofOfTestApi
 dotnet publish src/HolderAppApi/HolderAppApi.csproj -o publish/HolderAppApi
 dotnet publish src/VerifierAppApi/VerifierAppApi.csproj -o publish/VerifierAppApi
 dotnet publish src/CmsSigner/CmsSigner.csproj -o publish/Tools/CmsSigner -p:PublishProfile=Publish-Linux-x64
