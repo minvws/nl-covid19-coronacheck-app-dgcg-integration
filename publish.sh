02b454330bba4a663823d3724407d093e73cfe90
Ryan Barrett <ryan@seldonplan.com>
2021-03-18 09:16:45 +0100

    Added the new project to ther build

--- publish.sh
+++ publish.sh
@@ -6,6 +6,10 @@ dotnet publish src/IssuerApi/IssuerApi.csproj -o publish/IssuerApi
 tar -czvf publish/IssuerApi.tar.gz publish/IssuerApi
 rm -r publish/IssuerApi
 
+dotnet publish src/StaticProofApi/StaticProofApi.csproj -o publish/StaticProofApi
+tar -czvf publish/StaticProofApi.tar.gz publish/StaticProofApi
+rm -r publish/StaticProofApi
+
 dotnet publish src/CmsSigner/CmsSigner.csproj -o publish/Tools/CmsSigner
 tar -czvf publish/CmsSigner.tar.gz publish/Tools/CmsSigner
 rm -r publish/Tools/CmsSigner
