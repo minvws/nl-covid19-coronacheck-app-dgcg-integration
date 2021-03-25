06f7686e5bc412a4b147098fe0a10639148cc329
Ryan Barrett <ryan@seldonplan.com>
2021-03-25 09:49:00 +0100

    Removed old artifacts from the build scripts

--- build.sh
+++ build.sh
@@ -2,6 +2,3 @@ dotnet build src/ProofOfTestApi/ProofOfTestApi.csproj
 dotnet build src/IssuerApi/IssuerApi.csproj
 dotnet build src/StaticProofApi/StaticProofApi.csproj
 dotnet build src/CmsSigner/CmsSigner.csproj
-
-#dotnet build src/HolderAppApi/HolderAppApi.csproj
-#dotnet build src/VerifierAppApi/VerifierAppApi.csproj
